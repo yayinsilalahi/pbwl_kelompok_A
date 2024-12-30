8fe6378761deb4d8272f5bcb4e57f16a2131ee49
yayinsilalahi <yayinauliasilalahi@gmail.com>
2024-12-30 22:33:56 +0700

    Membuat file blade untuk Landing, Home, About, Feedback, dan Admin, Membuat navbar dan template tampilan awal website pada Home, Mengcopy file Home menjadi Template untuk Halaman berikutnya

---
+++ resources/views/template.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+{{-- STYLE --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abyssinica+SIL&family=Questrial&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="template.css">
+{{-- STYLE --}}
+</head>
+<body style="background: linear-gradient(to right, #F67280 50%, #E6B2C6 50%)">
+        <div class="container">
+            <div class="row">
+                <nav class="navbar">
+                    <div class="col-sm-6">
+                        <img src="images/logo-donat.png" alt="" width="150px">
+                    </div>
+                    <div class="nav-link">
+                        <a class="nav-link" href="/home">Home</a>
+                        <a class="nav-link" href="/about">About</a>
+                        <a class="nav-link" href="/feedback">Feedback</a>
+                    </div>
+                </nav>
+            </div>
+            <div class="row1">
+                <img class="gambar1" src="images/donat1.png" alt="" width="500px">
+                <div class="welcome">W E L C O M E</div>
+                <div class="sosmed">
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-tiktok"></i>
+                    <i class="fa-brands fa-whatsapp"></i>
+                </div>
+                <div class="donat2"><img src="images/donat2.png" alt="" width="150px"></div>
+                <div class="garis-vertikal"><img src="images/garis1.png" alt="" width="35px"></div>
+            </div>
+        </div>
+        <div class="row2">
+            <div class="layer2">Halo</div>
+        </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
